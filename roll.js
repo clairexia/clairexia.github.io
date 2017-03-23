85a0f861f8d54f4f9548aa9720779cbd2a3211cb
clairexia <clairexia@163.com>
2017-03-23 19:01:39 +0800

    Create roll.js

---
+++ roll.js
@@ -0,0 +1,56 @@
+
+
+var area = document.getElementById('scroll_list');
+
+
+ var iliHeight = 22;//单行滚动的高度
+
+ 
+ var speed = 50;//滚动的速度
+
+ var time;
+
+ var delay= 2000;
+
+ area.scrollTop=0;
+
+ area.innerHTML+=area.innerHTML;//克隆一份一样的内容
+
+function startScroll(){
+
+	 time=setInterval("scrollUp()",speed);
+
+	 area.scrollTop++;
+
+	 }
+
+ function scrollUp(){
+
+	 if(area.scrollTop % iliHeight==0){
+
+		 clearInterval(time);
+
+		 setTimeout(startScroll,delay);
+
+		 }else{
+
+			 area.scrollTop++;
+
+			 if(area.scrollTop >= area.scrollHeight/2){
+
+				 area.scrollTop =0;
+
+				 }
+
+			 }
+
+	 }
+
+
+	 setTimeout(startScroll,delay);
+// area.onmouseover = function(){
+// 	 clearInterval(time2);
+// 	}
+// area.onmouseout = function(){
+// 	 time2 = setInterval("scrollUp()",speed);
+// 	}
